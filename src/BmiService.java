068965249f9e57e686dedd3e924bc240585026ff
ArtOxid <bashkin.artem@gmail.com>
2022-10-27 13:21:34 +0600

    second commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,6 @@
 public class BmiService {
-    public int index() {
-        int weight = 70;
-        double hight = 1.60;
-        int index = (int) (weight / (hight * hight));
-        return index;
+    public int index(int weight, double hight) {
+        double index = weight / (hight * hight);
+        return (int) index;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int index = service.index();
+        int weight = 70;
+        double hight = 1.60;
+        int index = service.index(weight, hight);
         System.out.println(index);
     }
 }
